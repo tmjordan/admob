41f50c208065ac2d7a04f14d69ea9a75e1b1c899
tmjordan <jordantchokonte@yahoo.fr>
2021-11-03 00:03:48 +0100

    Create AdEventType.ts

---
+++ src/AdEventType.ts
@@ -0,0 +1,8 @@
+export const AdEventType = {
+    CLICKED: 'clicked',
+    CLOSED: 'closed',
+    ERROR: 'error',
+    LEFT_APPLICATION: 'left_application',
+    LOADED: 'loaded',
+    OPENED: 'opened',
+};
